d7776de1fcb05a04ab97525ec62298d55fbe5630
Tarik1112 <tare.mujke@gmail.com>
2021-04-10 12:32:03 +0200

    Button functions added

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
-function randomValue(min, max){
-    return Math.floor(Math.random() * (max - min)) + min;
+function randomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
 
 const app = Vue.createApp({
-    data(){
-        return{
-            playerHP: 100,
-            monsterHP: 100,
-        };
-    },
-    methods:{
-        monsterAttack(){
-            const damage = randomValue(8, 14);
-            this.monsterHP -= damage;
-            this.playerAttack();
-        },
-        playerAttack(){
-            const damage = randomValue(10,18);
-            this.playerHP -= damage;
+  data() {
+    return {
+      playerHP: 100,
+      monsterHP: 100,
+      counter: 0,
+    };
+  },
+  computed: {
+    monsterBar() {
+        if(this.monsterHP < 0){
+            return {width: '0%'}
+        }else{
+            return { width: this.monsterHP + "%" };
         }
     },
-    computed:{
-        monsterBar(){
-            return{width: this.monsterHP + '%'}
-        },
-        playerBar(){
-            return{width: this.playerHP + '%'}
+    playerBar() {
+        if(this.playerHP < 0){
+            return {width: '0%'}
+        }else{
+            return { width: this.playerHP + "%" };
         }
     },
-    watch:{
+    specialUsing() {
+      return this.counter % 3 !== 0;
+    },
+  },
+  methods: {
+    monsterAttack() {
+      this.counter++;
+      const damage = randomValue(8, 14);
+      this.monsterHP -= damage;
+      this.playerAttack();
+    },
+    playerAttack() {
+      const damage = randomValue(10, 18);
+      this.playerHP -= damage;
+    },
+    playerHeal() {
+      this.counter++;
+      const heal = randomValue(15, 20);
+      this.playerHP += heal;
+      this.playerAttack();
+    },
+    specialAttack() {
+      this.counter++;
+      const damage = randomValue(15, 30);
+      this.monsterHP -= damage;
+      this.playerAttack();
+    },
+  },
+  watch: {},
+});
 
-    }
-})
-
-app.mount('#game');
+app.mount("#game");
