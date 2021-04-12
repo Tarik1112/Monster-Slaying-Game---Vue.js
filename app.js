b28dbb62bdf7c50c7ebb2576b0e1fa71a76b5a1e
Tarik1112 <tare.mujke@gmail.com>
2021-04-12 15:37:32 +0200

    Added Game Over and surrender button

--- app.js
+++ app.js
@@ -8,37 +8,58 @@ const app = Vue.createApp({
       playerHP: 100,
       monsterHP: 100,
       counter: 0,
+      winner: null,
+      
     };
   },
   computed: {
     monsterBar() {
         if(this.monsterHP < 0){
             return {width: '0%'}
-        }else{
-            return { width: this.monsterHP + "%" };
         }
+        return { width: this.monsterHP + "%" };
     },
     playerBar() {
         if(this.playerHP < 0){
             return {width: '0%'}
-        }else{
-            return { width: this.playerHP + "%" };
         }
+        return { width: this.playerHP + "%" };
     },
     specialUsing() {
       return this.counter % 3 !== 0;
     },
+  },
+  watch:{
+    playerHP(value){
+      if(value <= 0 && this.monsterHP <= 0){
+          this.winner = 'draw';
+      }else if(value <= 0){
+          this.winner = 'monster';
+      }
+    },
+    monsterHP(value){
+      if(value <= 0 && this.playerHP <= 0){
+          this.winner = 'draw';
+      }else if(value <= 0){
+          this.winner = 'player';
+      }
+    }
   },
   methods: {
     monsterAttack() {
       this.counter++;
       const damage = randomValue(8, 14);
       this.monsterHP -= damage;
+     // console.log(this.monsterHP);
+      
       this.playerAttack();
     },
     playerAttack() {
       const damage = randomValue(10, 18);
       this.playerHP -= damage;
+      
+
+     // console.log(this.playerHP);
     },
     playerHeal() {
       this.counter++;
@@ -52,8 +73,19 @@ const app = Vue.createApp({
       this.monsterHP -= damage;
       this.playerAttack();
     },
+    startNewGame(){
+      this.playerHP = 100;
+      this.monsterHP = 100;
+      this.counter = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
+    surrenderGame(){
+      this.winner = "monster";
+    },
+    
   },
-  watch: {},
+  
 });
 
 app.mount("#game");
